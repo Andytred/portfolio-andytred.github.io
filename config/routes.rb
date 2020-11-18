4baf41288a204e80aedd021a3bf43727fd9c4177
Andrea Tredrea <andrea.tredrea@gmail.com>
2020-11-18 13:59:23 +0100

    creating first project page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'first', to: 'pages#first'
+  get 'second', to: 'pages#second'
 end
