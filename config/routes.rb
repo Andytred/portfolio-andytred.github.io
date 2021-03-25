193b8776e39269ef5825a045f1509fe3557caba5
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-03-25 09:39:47 +0100

    changes in portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'first', to: 'pages#first'
   get 'second', to: 'pages#second'
+  get 'third', to: 'pages#third'
 end
